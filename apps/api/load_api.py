f268c80c27ea27d80afa69f61dfd3ba55dad0829
NVERRON <nicoverron@hotmail.com>
2022-09-03 18:44:46 +0200

    feat: Utilisation de flask_restful

---
+++ apps/api/load_api.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+
+from apps.shared import api
+from apps.api.routes import Routes
+
+from apps.api.user_api import UserApi, UserListApi
+
+load_api_blueprint = Blueprint('load_api_page', __name__, static_folder='static', template_folder='templates')
+
+api.add_resource(UserApi, Routes.USER, Routes.USER_ID)
+api.add_resource(UserListApi, Routes.USERS)
