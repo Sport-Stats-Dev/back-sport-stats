36399f74e45c59dedeb873f8f63adb39f9e7e7d4
NVERRON <nicoverron@hotmail.com>
2023-08-05 20:29:49 +0200

    nouvelle stat 'poid max'

--- apps/api/stats_api.py
+++ apps/api/stats_api.py
@@ -1,3 +1,6 @@
+from flask_restful import request
+
+from datetime import datetime
 from apps.core.core_resource import AuthResource
 import apps.controller.workout_controller as workout_controller
 from apps.model.execution import Execution
@@ -22,8 +25,14 @@ class VolumesEvolApi(AuthResource):
         return _get_data(exercise_id, Execution.get_volume, isEvolution=True), 200
 
 
+class MaxWeightApi(AuthResource):
+    def get(self, exercise_id):
+        return _get_data(exercise_id, Execution.get_max_weight), 200
+
+
 def _get_data(exercise_id, func, isEvolution=False):
-    executions = workout_controller.get_executions_by_exercise(exercise_id=exercise_id)
+    periodStart = request.args.get("periodStart", None, type=int)
+    executions = workout_controller.get_executions_by_exercise(exercise_id=exercise_id, periodStart=periodStart)
     data = []
     average = None
 
@@ -36,8 +45,10 @@ def _get_data(exercise_id, func, isEvolution=False):
 
         data.append({
             "execution_id": e.id,
-            "date": str(e.workout.date),
+            "date": datetime.timestamp(e.workout.date),
             "value": value
         })
+
+        data.sort(key=lambda x: x["date"])
 
     return data
