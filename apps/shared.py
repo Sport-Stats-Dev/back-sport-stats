3fad01ba1f60b387222aa8d5e431bfdf7bb6a330
NVERRON <nicoverron@hotmail.com>
2023-11-01 13:13:45 +0100

    ajout du type de série

--- apps/shared.py
+++ apps/shared.py
@@ -1,11 +1,13 @@
 from flask import g
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from werkzeug.local import LocalProxy
 
 
 db = None
 ma = None
+migrate = None
 
 current_user = LocalProxy(lambda: g.get('_current_user'))
 
@@ -14,3 +16,4 @@ def init_app(app):
     global db, ma
     db = SQLAlchemy(app)
     ma = Marshmallow(app)
+    migrate = Migrate(app, db)
